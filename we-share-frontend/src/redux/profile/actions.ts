8a18290c232768be37a7d3282c7044b8179cc8cc
lukmingchuelise <ymsmemories@gmail.com>
2018-07-02 23:10:38 +0800

    first commit

--- we-share-frontend/src/redux/profile/actions.ts
+++ we-share-frontend/src/redux/profile/actions.ts
@@ -16,121 +16,121 @@ export type LOAD_CONTRACTDETAIL = typeof LOAD_CONTRACTDETAIL;
 
 
 export interface IProfilePostAction {
-    type: LOAD_PROFILEPOST;
-    profilePost: IProfilePostData[];
+   type: LOAD_PROFILEPOST;
+   profilePost: IProfilePostData[];
 }
 
 export interface IProfileRatingAction {
-    type: LOAD_PROFILERATING;
-    profileRating: IProfileRatingData[];
+   type: LOAD_PROFILERATING;
+   profileRating: IProfileRatingData[];
 }
 
 export interface IProfileContractAction {
-    type: LOAD_PROFILECONTRACT;
-    profileContract: IProfileContractData[];
+   type: LOAD_PROFILECONTRACT;
+   profileContract: IProfileContractData[];
 }
 
 export interface IContractDetailAction {
-    type: LOAD_CONTRACTDETAIL;
-    contractDetail: IContractDetail[];
+   type: LOAD_CONTRACTDETAIL;
+   contractDetail: IContractDetail[];
 }
 
 export type IProfileDataAction = IProfilePostAction | IProfileRatingAction | IProfileContractAction | IContractDetailAction;
 
 
 export function getProfilePostData(profilePost: IProfilePostData[]): IProfileDataAction {
-    return {
-        // tslint:disable-next-line:object-literal-shorthand
-        profilePost,
-        type: LOAD_PROFILEPOST
-    };
+   return {
+       // tslint:disable-next-line:object-literal-shorthand
+       profilePost,
+       type: LOAD_PROFILEPOST
+   };
 }
 
 export function getProfileRatingData(profileRating: IProfileRatingData[]): IProfileDataAction {
-    return {
-        // tslint:disable-next-line:object-literal-shorthand
-        profileRating,
-        type: LOAD_PROFILERATING
-    };
+   return {
+       // tslint:disable-next-line:object-literal-shorthand
+       profileRating,
+       type: LOAD_PROFILERATING
+   };
 }
 
 export function getProfileContractData(profileContract: IProfileContractData[]): IProfileDataAction {
-    // tslint:disable-next-line:no-console
-    return {
-        // tslint:disable-next-line:object-literal-shorthand
-        profileContract,
-        type: LOAD_PROFILECONTRACT
-    };
+   // tslint:disable-next-line:no-console
+   return {
+       // tslint:disable-next-line:object-literal-shorthand
+       profileContract,
+       type: LOAD_PROFILECONTRACT
+   };
 }
 
 export function getContractDetailData(contractDetail: IContractDetail[]): IProfileDataAction {
-    // tslint:disable-next-line:no-console
-    return {
-        // tslint:disable-next-line:object-literal-shorthand
-        contractDetail,
-        type: LOAD_CONTRACTDETAIL
-    };
+   // tslint:disable-next-line:no-console
+   return {
+       // tslint:disable-next-line:object-literal-shorthand
+       contractDetail,
+       type: LOAD_CONTRACTDETAIL
+   };
 }
 
 
 export function fetchProfilePostData(userid: number) {
-    // console.log(userid);
-    return (dispatch: Dispatch<IProfileDataAction>) => {
-        axios
-            .post(`${process.env.REACT_APP_API_SERVER}/api/profile`, {
-                userid,
-            }).then(res => {
-                // tslint:disable-next-line:no-console
-                // console.log(res.data)
-                dispatch(getProfilePostData(res.data))
-            }).catch(err => {
-                // tslint:disable-next-line:no-console
-                console.log(err)
-            });
+   // console.log(userid);
+   return (dispatch: Dispatch<IProfileDataAction>) => {
+       axios
+           .post(`${process.env.REACT_APP_API_SERVER}/api/profile`, {
+               userid,
+           }).then(res => {
+               // tslint:disable-next-line:no-console
+               // console.log(res.data)
+               dispatch(getProfilePostData(res.data))
+           }).catch(err => {
+               // tslint:disable-next-line:no-console
+               console.log(err)
+           });
 
 
-        axios
-            .post(`${process.env.REACT_APP_API_SERVER}/api/profile/ratingdata`, {
-                userid,
-            }).then(res => {
-                // tslint:disable-next-line:no-console
-                console.log(res.data)
-                dispatch(getProfileRatingData(res.data))
-            }).catch(err => {
-                // tslint:disable-next-line:no-console
-                console.log(err)
-            });
+       axios
+           .post(`${process.env.REACT_APP_API_SERVER}/api/profile/ratingdata`, {
+               userid,
+           }).then(res => {
+               // tslint:disable-next-line:no-console
+               console.log(res.data)
+               dispatch(getProfileRatingData(res.data))
+           }).catch(err => {
+               // tslint:disable-next-line:no-console
+               console.log(err)
+           });
 
 
-            axios
-            .post(`${process.env.REACT_APP_API_SERVER}/api/profile/contractdata`, {
-                userid,
-            }).then(res => {
-                // tslint:disable-next-line:no-console
-                console.log(res.data)
-                dispatch(getProfileContractData(res.data))
-            }).catch(err => {
-                // tslint:disable-next-line:no-console
-                console.log(err)
-            });
-    };
+           axios
+           .post(`${process.env.REACT_APP_API_SERVER}/api/profile/contractdata`, {
+               userid,
+           }).then(res => {
+               // tslint:disable-next-line:no-console
+               console.log(res.data)
+               dispatch(getProfileContractData(res.data))
+           }).catch(err => {
+               // tslint:disable-next-line:no-console
+               console.log(err)
+           });
+   };
 }
 
 
 export function contractDetailData(contractId: number) {
-    // // tslint:disable-next-line:no-console
-    // console.log(contractId);
-    return (dispatch: Dispatch<IProfileDataAction>) => {
-        axios
-            .post(`${process.env.REACT_APP_API_SERVER}/api/profile/contract_detail_data`, {
-                contractId,
-            }).then(res => {
-                // tslint:disable-next-line:no-console
-                console.log(res.data)
-                dispatch(getContractDetailData(res.data))
-            }).catch(err => {
-                // tslint:disable-next-line:no-console
-                console.log(err)
-            });
-    };
+   // // tslint:disable-next-line:no-console
+   // console.log(contractId);
+   return (dispatch: Dispatch<IProfileDataAction>) => {
+       axios
+           .post(`${process.env.REACT_APP_API_SERVER}/api/profile/contract_detail_data`, {
+               contractId,
+           }).then(res => {
+               // tslint:disable-next-line:no-console
+               console.log(res.data)
+               dispatch(getContractDetailData(res.data))
+           }).catch(err => {
+               // tslint:disable-next-line:no-console
+               console.log(err)
+           });
+   };
 }
