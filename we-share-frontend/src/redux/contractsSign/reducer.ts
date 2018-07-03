642240b4cf0bf437b077b02f5dcbda8158c2ae22
Your Name <you@example.com>
2018-07-03 13:27:43 +0800

    commit

--- we-share-frontend/src/redux/contractsSign/reducer.ts
+++ we-share-frontend/src/redux/contractsSign/reducer.ts
@@ -1,41 +1,43 @@
 import { IFetchContractsAction, ILoadContractsData, LOAD_CONTRACTS, USER_CONTRACTS_CONFIRMATION } from "./actions";
 
 export interface ISignContractsState {
- 
-   loadContracts:ILoadContractsData[];
-   is_agree: boolean;
- }
+
+  loadContracts: ILoadContractsData[];
+  is_agree: boolean;
+}
 
 const initialState = {
-   loadContracts: [],
-   // tslint:disable-next-line:object-literal-sort-keys
-   is_agree: false,
+  loadContracts: [],
+  // tslint:disable-next-line:object-literal-sort-keys
+  is_agree: false,
 };
-  export const reducer = (oldState: ISignContractsState = initialState, action: IFetchContractsAction) => {
-   switch (action.type) {
-     case LOAD_CONTRACTS:
-     {
-     const loadContracts = oldState.loadContracts.concat({
-       contractId:action.loadContracts.contractId,
-       productName: action.loadContracts.productName,
-       // tslint:disable-next-line:object-literal-sort-keys
-       price: action.loadContracts.price,
-       participants: action.loadContracts.participants,
-       description: action.loadContracts.description
-   });
-       return { ...oldState, loadContracts };
+export const reducer = (oldState: ISignContractsState = initialState, action: IFetchContractsAction) => {
+  switch (action.type) {
+    case LOAD_CONTRACTS:
+      {
+        const loadContracts = oldState.loadContracts.filter(loadContract => loadContract.contractId !== action.loadContracts.contractId);
+        loadContracts.push({
+          contractId: action.loadContracts.contractId,
+          productName: action.loadContracts.productName,
+          // tslint:disable-next-line:object-literal-sort-keys
+          price: action.loadContracts.price,
+          participants: action.loadContracts.participants,
+          description: action.loadContracts.description
+        });
+        return { ...oldState, loadContracts };
+
       }
-      case USER_CONTRACTS_CONFIRMATION:
+    case USER_CONTRACTS_CONFIRMATION:
       {
         return {
           ...oldState,
           is_agree: action.is_agree
         };
-      } 
-     default:
-       return oldState;
-   }
- };
+      }
+    default:
+      return oldState;
+  }
+};
 
 
 
